2a54fd3023f9afd8fec13d32d503ec5ed3debd7a
Matheus Figueiredo Lima <47124219+Mattst123@users.noreply.github.com>
2020-11-21 10:55:53 -0300

    1

---
+++ src/test/java/com/refugiados/web_refugiados/WebRefugiadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refugiados.web_refugiados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRefugiadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
